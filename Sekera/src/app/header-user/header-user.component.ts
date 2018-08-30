50c471a13276bea68aaba653a7daaf9b88584487
Peter Krak <pkrak@csas.cz>
2018-08-30 14:58:42 +0200

    .
    .

--- Sekera/src/app/header-user/header-user.component.ts
+++ Sekera/src/app/header-user/header-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-header-user',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header-user.component.css']
 })
 export class HeaderUserComponent implements OnInit {
+  user_id: number;
 
-  constructor() { }
+
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
   }
+
+
+
+getUserResource() {
+  this.httpClient.post(`http://127.0.0.1:5000/users/resources/${this.user_id}`, {
+
+  })
+}
 
 }
--- Sekera/src/app/login/login.component.ts
+++ Sekera/src/app/login/login.component.ts
@@ -32,7 +32,7 @@ export class LoginComponent implements OnInit {
 
   postSignup() {
     if (this.username != '' && this.password != '')
-      this.httpClient.post(`http://127.0.0.1:5000/auth/signup`, {
+      this.httpClient.post(`http://127.0.0.1:5000/auth/signup/`, {
         "username": this.username,
         "password": this.password
       })
